ba4e55cc9bac84b2dca903fcc36c5b9fd2b24d22
sv.junic <sv.junic@gmail.com>
2018-05-30 19:14:05 +0900

    古いファイル削除

--- resource/main.js
+++
@@ -1,7 +0,0 @@
-document.write("It works.");
-
-base = require( './modules/module1' );
-
-base.echo( 'sanma' );
-
-document.write("It works.");
--- webpack.config.js
+++
@@ -1,4 +0,0 @@
-/*
- * 参考：https://github.com/sasaplus1-prototype/webpack-base-config/blob/master/webpack.config.js
- */
-require('coffee-script/register') && (module.exports = require('./webpack.config.coffee'));
