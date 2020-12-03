e71c7a4e097c6e1d8ca173b2938c6c75c351168f
svjunic <sv.junic@gmail.com>
2020-12-03 16:22:27 +0900

    hoge

--- _frontend/webpack.config.js
+++ _frontend/webpack.config.js
@@ -2,58 +2,46 @@ console.log(process.env.NODE_ENV);
 
 const path = require('path');
 const webpack = require('webpack');
-const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 
 const DOCUMENT_ROOT = '../docs/';
 const RESOURCES_ROOT = './resources/page/';
 
-module.exports = function() {
+const isProduction = process.env.NODE_ENV === 'production';
+
+module.exports = function () {
+  const mode = isProduction ? 'production' : 'development';
+
   const entries = {
-    'js/index': path.join(__dirname, `${RESOURCES_ROOT}js/index.js`)
+    'js/index': path.join(__dirname, `${RESOURCES_ROOT}js/index.js`),
   };
 
   const watchOptions = {
-    poll: true
+    poll: true,
   };
 
   const output = {
     path: path.join(__dirname, DOCUMENT_ROOT),
     filename: '[name].js',
     chunkFilename: '[name].js',
-    jsonpFunction: 'sandbox'
+    uniqueName: 'sandbox',
   };
 
   let optimization = {
     splitChunks: {
       name: 'js/vendor',
       //chunks: 'initial'
-      chunks: 'all'
-    }
+      chunks: 'all',
+    },
   };
-
-  if (process.env.NODE_ENV === 'production') {
-    optimization = Object.assign(optimization, {
-      minimizer: [
-        new UglifyJsPlugin({
-          uglifyOptions: {
-            warnings: false,
-            compress: {
-              drop_console: true
-            }
-          }
-        })
-      ]
-    });
-  }
 
   const resolve = {
     // aliasを指定するとグローバルに展開されない
     alias: {
       //'jquery': 'jquery/src/jquery',
-      'velocity-animate': 'velocity-animate/velocity.min.js'
+      'velocity-animate': 'velocity-animate/velocity.min.js',
     },
     // モジュール検索
-    modules: [path.resolve(__dirname, 'resources'), 'node_modules']
+    modules: [path.resolve(__dirname, 'resources'), 'node_modules'],
   };
 
   const baseConfig = {
@@ -66,30 +54,27 @@ module.exports = function() {
           exclude: /node_modules/,
           use: [
             {
-              loader: 'babel-loader'
-            }
-          ]
-        }
-      ]
+              loader: 'babel-loader',
+            },
+          ],
+        },
+      ],
     },
 
-    plugins: [
-      new webpack.NoEmitOnErrorsPlugin(),
-      new webpack.optimize.AggressiveMergingPlugin()
-    ]
+    plugins: [new webpack.NoEmitOnErrorsPlugin(), new webpack.optimize.AggressiveMergingPlugin()],
   };
 
   return [
     Object.assign(
       {
-        mode: 'production',
+        mode: mode,
         watchOptions: watchOptions,
         entry: entries,
         resolve: resolve,
         output: output,
-        optimization: optimization
+        optimization: optimization,
       },
       baseConfig
-    )
+    ),
   ];
 };
